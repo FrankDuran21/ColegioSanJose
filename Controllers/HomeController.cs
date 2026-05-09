434f1c701616a42e20ac1bb5e3d4243fcb36a527
Frank Duran <franciscoduran2112@hotmail.com>
2026-05-09 14:14:29 -0600

    Se agrega dashboard principal del sistema

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,13 +1,37 @@
+using ColegioSanJose.Data;
 using ColegioSanJose.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace ColegioSanJose.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
+            _logger = logger;
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.TotalAlumnos = await _context.Alumnos.CountAsync();
+            ViewBag.TotalMaterias = await _context.Materias.CountAsync();
+            ViewBag.TotalExpedientes = await _context.Expedientes.CountAsync();
+
+            decimal promedioGeneral = 0;
+
+            if (await _context.Expedientes.AnyAsync())
+            {
+                promedioGeneral = await _context.Expedientes.AverageAsync(e => e.NotaFinal);
+            }
+
+            ViewBag.PromedioGeneral = promedioGeneral;
+
             return View();
         }
 
@@ -19,7 +43,10 @@ namespace ColegioSanJose.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new ErrorViewModel
+            {
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            });
         }
     }
 }
